96ea08a55b4e6f449a956808dcd480b53b67bf7b
milindanath <milindanath@gmail.com>
2015-11-28 00:06:27 +0100

    added angular

---
+++ DhammaStore/Scripts/Site_Angular/Modules/main.js
@@ -0,0 +1,9 @@
+﻿(function() {
+    var dhammaApp = angular.module("dhammaStore", []);
+
+    var dhammaController = function($scope, $http) {
+        $scope.message = "Welcome to the dhamma store web page";
+    };
+
+    dhammaApp.controller("DhammaController", dhammaController);
+}())
